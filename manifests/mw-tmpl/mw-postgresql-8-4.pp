864e5e890f19bef6fa66dc43785a72bb678c2ffe
Jerome Bove <jerome.bove@camptocamp.com>
2011-03-18 16:07:40 +0100

    Create nodes and srv-tmpl for openerp-v5-squeeze.int.lsn and openerp-v6-squeeze.int.lsn

--- manifests/mw-tmpl/mw-postgresql-8-4.pp
+++ manifests/mw-tmpl/mw-postgresql-8-4.pp
@@ -3,8 +3,11 @@ class generic-tmpl::mw-postgresql-8-4 {
   include postgresql::v8-4
   include postgresql::administration
 
-  package {"python-psycopg2":
-    ensure => present,
+  if !defined(Package["python-psycopg2"]) {
+        package {"python-psycopg2": 
+          ensure => present,
+        }
   }
+
 
 }
