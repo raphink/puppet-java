73dcd629cf0f811e9715fedb8e2c0a4188fb02b6
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2011-05-10 10:36:08 +0200

    generic-tmpl::mw-postgis-8-4: also include c2c sig and sig-nonfree repos for squeeze.

--- manifests/mw-tmpl/mw-postgis-8-4.pp
+++ manifests/mw-tmpl/mw-postgis-8-4.pp
@@ -8,29 +8,31 @@ class generic-tmpl::mw-postgis-8-4 {
       }
     }
 
-    Apt::Preferences["postgresql-8.4-postgis"] {
-      pin => "release o=Camptocamp",
-    }
-
     Exec["create postgis_template"] {
       require +> Class["mw-postgresql-8-4"],
     }
 
-    Package["postgis"] {
-      require +> [
-        Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"],
-        Apt::Key["5C662D02"],
-      ]
+    if $lsbdistcodename == "lenny" {
+      Apt::Preferences["postgresql-8.4-postgis"] {
+        pin => "release o=Camptocamp",
+      }
+
+      Package["postgis"] {
+        require +> [
+          Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"],
+          Apt::Key["5C662D02"],
+        ]
+      }
     }
   }
 
   case $lsbdistcodename {
     # Debian
     lenny :  { include c2c-postgis }
-    squeeze :  { include postgis }
+    squeeze :  { include c2c-postgis }
 
     # Ubuntu
-    lucid : { include postgis }
+    lucid : { include c2c-postgis }
 
     default: { fail "mw-postgis-8-4 not supported for ${operatingsystem}/${lsbdistcodename}"}
   }
