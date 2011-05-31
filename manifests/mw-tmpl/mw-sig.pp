f6a304d22b99a6227937920afaea9c4d9e9db71a
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2011-05-31 11:57:36 +0200

    generic-tmpl::mw-sig: added mapserver 6 support

--- manifests/mw-tmpl/mw-sig.pp
+++ manifests/mw-tmpl/mw-sig.pp
@@ -10,6 +10,11 @@
 
 class generic-tmpl::mw-sig {
 
+  # avoid partial configuration on untested-distribution
+  if $lsbdistcodename !~ /^(lenny|squeeze)$/ {
+    fail "${name} not tested on ${operatingsystem}/${lsbdistcodename}"
+  }
+
   class c2c-mapserver inherits mapserver::debian {
 
     if !defined(Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"]) {
@@ -20,53 +25,123 @@ class generic-tmpl::mw-sig {
         require => Apt::Key["5C662D02"],
       }
     }
-
-    apt::preferences{"sig":
-      package  => "*",
-      pin      => "release c=sig, o=Camptocamp",
-      priority => 1001,
-    }
-    apt::preferences{"sig-non-free":
-      package  => "*",
-      pin      => "release c=sig-non-free, o=Camptocamp",
-      priority => 1001,
-    }
-
-    apt::preferences{["libgeos-c1", "proj"]:
-      pin      => "release a=lenny-backports, o=Camptocamp",
-      priority => 1001,
-    }
-
-    case $mapserver_version {
-      default: { fail "Unsupported value for \$mapserver_version. Choices are 5.4 or 5.6." }
-      "","5.4": {
-        Package {
-          require => [ Exec["apt-get_update"],
-                       Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"],
-                       Apt::Preferences["sig", "sig-non-free", "libgeos-c1", "proj"],
-                     ],
-        }
-      }
-      "5.6": {
-        apt::sources_list {"c2c-${lsbdistcodename}-${repository}-mapserver-5.6":
-          ensure  => present,
-          content => "deb http://pkg.camptocamp.net/${repository} ${lsbdistcodename} mapserver-5.6\n",
-          require => Apt::Key["5C662D02"],
-        }
-        apt::preferences{"mapserver-5_6":
-          package  => "*",
-          pin      => "release c=mapserver-5.6",
+ 
+    case $lsbdistcodename {
+      squeeze: {
+        apt::preferences{[
+          "gdal-bin",
+          "libgdal-doc",
+          "libgdal-perl",
+          "libgdal-ruby",
+          "libgdal-ruby1.8",
+          "libgdal1-1.8.0",
+          "libgdal1-dev",
+          "python-gdal"]:
+          pin      => "version 1.8.0-1~c2c+*",
           priority => 1001,
         }
+
+        apt::preferences{[
+          "cgi-mapserver",
+          "libmapscript-ruby",
+          "libmapscript-ruby1.8",
+          "libmapscript-ruby1.9.1",
+          "mapserver-bin",
+          "mapserver-doc",
+          "perl-mapscript",
+          "php5-mapscript",
+          "python-mapscript"]:
+          pin => "version 6.0.0-1~c2c*",
+          priority => 1001,
+        }
+
+        apt::preferences{"libecw":
+          pin => "version 3.3-1+squeeze1~c2c*",
+          priority => 1001,
+        }
+
+        package {"libgdal1-1.7.0":
+          ensure => purged,
+        }
+
         Package {
-          require => [ Exec["apt-get_update"],
-                       Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"], Apt::Preferences["sig", "sig-non-free", "libgeos-c1", "proj"],
-                       Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-mapserver-5.6"], Apt::Preferences["mapserver-5.6"]
-                     ],
+          require => [
+            Exec["apt-get_update"], 
+            Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"],
+            Apt::Preferences[
+              "gdal-bin",
+              "libgdal-doc",
+              "libgdal-perl",
+              "libgdal-ruby",
+              "libgdal-ruby1.8",
+              "libgdal1-1.8.0",
+              "libgdal1-dev",
+              "python-gdal",
+              "cgi-mapserver",
+              "libmapscript-ruby",
+              "libmapscript-ruby1.8",
+              "libmapscript-ruby1.9.1",
+              "mapserver-bin",
+              "mapserver-doc",
+              "perl-mapscript",
+              "php5-mapscript",
+              "python-mapscript",
+              "libecw"
+            ],
+          ]
+        }   
+      }
+
+      lenny: {
+ 
+         apt::preferences{"sig":
+           package  => "*",
+           pin      => "release c=sig, o=Camptocamp",
+           priority => 1001,
+         }
+
+         apt::preferences{"sig-non-free":
+           package  => "*",
+           pin      => "release c=sig-non-free, o=Camptocamp",
+           priority => 1001,
+         }
+     
+         apt::preferences{["libgeos-c1", "proj"]:
+           pin      => "release a=lenny-backports, o=Camptocamp",
+           priority => 1001,
+         }
+     
+         case $mapserver_version {
+           default: { fail "Unsupported value for \$mapserver_version. Choices are 5.4 or 5.6." }
+           "","5.4": {
+             Package {
+               require => [ Exec["apt-get_update"],
+                            Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"],
+                            Apt::Preferences["sig", "sig-non-free", "libgeos-c1", "proj"],
+                          ],
+             }
+           }
+           "5.6": {
+             apt::sources_list {"c2c-${lsbdistcodename}-${repository}-mapserver-5.6":
+               ensure  => present,
+               content => "deb http://pkg.camptocamp.net/${repository} ${lsbdistcodename} mapserver-5.6\n",
+               require => Apt::Key["5C662D02"],
+             }
+             apt::preferences{"mapserver-5_6":
+               package  => "*",
+               pin      => "release c=mapserver-5.6",
+               priority => 1001,
+             }
+             Package {
+               require => [ Exec["apt-get_update"],
+                            Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"], Apt::Preferences["sig", "sig-non-free", "libgeos-c1", "proj"],
+                            Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-mapserver-5.6"], Apt::Preferences["mapserver-5.6"]
+                          ],
+            }
+          }
         }
       }
     }
-
   }
 
   case $operatingsystem {
