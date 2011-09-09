a75ab82c15bab4ba59327ba8e4a595e8da3830c0
Frederic Junod <frederic.junod@camptocamp.com>
2011-09-09 11:08:06 +0200

    generic-tmpl::mw-devel - install ack-grep

--- manifests/mw-tmpl/mw-devel.pp
+++ manifests/mw-tmpl/mw-devel.pp
@@ -1,6 +1,7 @@
 class generic-tmpl::mw-devel {
 
   package {
+    "ack-grep":      ensure => present; # a grep replacement
     "ipython":       ensure => present; # interactive python shell
     "libxml2-utils": ensure => present; # xml utilities
     "pyflakes":      ensure => present; # python static code checker
