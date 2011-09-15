1c8f64c6c650e23223e0e9b1fca8add576d72e69
Marc Fournier <marc.fournier@camptocamp.com>
2011-09-15 20:17:26 +0200

    mw-puppet-master-ecosystem: displays git module sync status in /etc/motd.

--- manifests/mw-tmpl/mw-puppet-master-ecosystem.pp
+++ manifests/mw-tmpl/mw-puppet-master-ecosystem.pp
@@ -3,4 +3,9 @@ class generic-tmpl::mw-puppet-master-ecosystem {
   include githubsync::dashboard
   include git-subtree
 
+  motd::message { "zzz-githubsync-status":
+    source  => "file:///var/local/run/githubsync/current-status.txt",
+    require => File["/var/local/run/githubsync/current-status.txt"],
+  }
+
 }
