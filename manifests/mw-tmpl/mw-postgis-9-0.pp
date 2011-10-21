08f3abde87cfbacc2d0a2922d502cd8ff3d340c8
Frederic Junod <frederic.junod@camptocamp.com>
2011-10-21 13:40:27 +0200

    generic-tmpl::mw-postgis-9-0 - pin the release (o=Camptocamp) not the packages version

--- manifests/mw-tmpl/mw-postgis-9-0.pp
+++ manifests/mw-tmpl/mw-postgis-9-0.pp
@@ -9,7 +9,7 @@ class generic-tmpl::mw-postgis-9-0 {
     }
 
     apt::preferences {["postgis", "postgresql-9.0-postgis"]:
-      pin      => "version 1.5.2-2~c2c+*",
+      pin      => "release o=Camptocamp",
       priority => "1100",
     }
 
