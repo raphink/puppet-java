fc9d72c223dbe9b5dc459f15239d20971a7e403b
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2011-11-17 09:24:07 +0100

    mw-postgresql-8-4: added apt::preferences for libpq-dev on lenny

--- manifests/mw-postgresql-8-4.pp
+++ manifests/mw-postgresql-8-4.pp
@@ -3,6 +3,13 @@ class generic-tmpl::mw-postgresql-8-4 {
   include postgresql::v8-4
   include postgresql::administration
 
+  if $lsbdistcodename == 'lenny' {
+    apt::preferences {'libpq-dev':
+      pin => 'release o=Camptocamp',
+      priority => 1100,
+    } 
+  } 
+
   if !defined(Package["python-psycopg2"]) {
     package {"python-psycopg2": 
       ensure => present,
