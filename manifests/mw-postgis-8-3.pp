61ed5aea4e14980692d8490601a3159d7cbeb349
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2011-12-21 09:14:38 +0100

    mw-postgis-8.3: use the default debian version of postgis

--- manifests/mw-postgis-8-3.pp
+++ manifests/mw-postgis-8-3.pp
@@ -2,17 +2,4 @@ class generic-tmpl::mw-postgis-8-3 {
 
   include postgis::debian::v8-3
 
-  if ! defined (Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"]) {
-    apt::sources_list {"c2c-${lsbdistcodename}-${repository}-sig":
-      ensure  => present,
-      content => "deb http://pkg.camptocamp.net/${repository} ${lsbdistcodename} sig sig-non-free",
-    }
-  }
-
-  apt::preferences { ["postgis", "postgresql-8.3-postgis"]:
-    pin      => "release o=Camptocamp",
-    priority => "1100",
-    before   => [Package["postgresql-postgis"], Package["postgis"]],
-  }
-
 }
