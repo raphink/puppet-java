26f81c4a871f38c5228e6a23d6473736937308c8
Marc Fournier <marc.fournier@camptocamp.com>
2012-01-17 17:05:46 +0100

    puppet: versions no-longer hardcoded. In reprepro we trust !

--- manifests/mw-puppet-client.pp
+++ manifests/mw-puppet-client.pp
@@ -11,54 +11,26 @@
 # Todo:
 #  - Intégrer également les distributions RedHat et Centos
 #
-class generic-tmpl::mw-puppet-client inherits puppet::client {
+class generic-tmpl::mw-puppet-client inherits puppet::client::base {
 
-  $puppet_client_version = $operatingsystem ? {
-    RedHat => "0.25.5-1.el${lsbmajdistrelease}",
-    Debian => "0.25.5-1~c2c+1",
-  }
-
-  $facter_version = $operatingsystem ? {
-    RedHat => "1.5.8-1.el${lsbmajdistrelease}",
-    Debian => $lsbdistcodename ? {
-      lenny   => "1.5.7-1~c2c+3",
-      default => "",
-    }
-  }
+  Package["facter"] { ensure => present }
+  Package["puppet"] { ensure => present }
 
   case $operatingsystem {
     Debian: {
-      case $lsbdistcodename {
-        /lenny|squeeze/: {
 
-          if ($facter_version != "") {
-            apt::preferences {"facter":
-              ensure   => present, 
-              pin      => "version ${facter_version}", 
-              priority => 1100;
-            }
-            Package["facter"] {
-              require +> Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sysadmin"],
-            }
-          }
-
-          apt::preferences {["puppet", "puppet-common","vim-puppet", "puppet-el"]:
-            ensure   => present,
-            pin      => "version ${puppet_client_version}",
-            priority => 1100;
-          }
-          package {["puppet-common","vim-puppet", "puppet-el"]:
-            ensure  => $puppet_client_version,
-            require => Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sysadmin"],
-            tag     => "install-puppet",
-          }
-          
-          Package["puppet"] {
-            require +> Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sysadmin"],
-          }
-     
-        }
+      apt::preferences {"facter":
+        ensure   => present,
+        pin      => "release o=Camptocamp",
+        priority => 1100,
       }
+
+      apt::preferences {["puppet", "puppet-common"]:
+        ensure   => present,
+        pin      => "release o=Camptocamp",
+        priority => 1100,
+      }
+
     }
-  } 
+  }
 }
