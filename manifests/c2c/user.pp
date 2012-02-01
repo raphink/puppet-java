a35a1e7914306d4fcbe411e948b73f70fce8e21b
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2012-02-01 11:54:31 +0100

    generic-tmpl::c2c::user: new definition to configure c2c users access

---
+++ manifests/c2c/user.pp
@@ -0,0 +1,54 @@
+define generic-tmpl::c2c::user (
+  $ensure   = present,
+  $username = false,
+  $groups   = false,
+  $onuser   = false,
+  $target   = false
+) {
+
+  # If ssh key type is 'none', the user is removed
+  $_ensure = $type ? {
+    'none'  => false,
+    default => $ensure,
+  }
+
+  $_username = $username ? {
+    false   => $name,
+    default => $username,
+  }
+
+  $_onuser = $onuser ? {
+    false   => $_username,
+    default => $onuser,
+  }
+
+  $email = url_get("${sadb}/user/${_username}/email")
+  $type  = url_get("${sadb}/user/${_username}/ssh_pub_key_type")
+  $key   = url_get("${sadb}/user/${_username}/ssh_pub_key")
+
+
+  if ($_onuser == $_username) and (!defined(User[$_username])) {
+
+    $firstname = url_get("${sadb}/user/${_username}/firstname")
+    $lastname  = url_get("${sadb}/user/${_username}/lastname")
+  
+    user {$_username:
+      ensure     => $_ensure,
+      comment    => "${firstname} ${lastname}",
+      uid        => url_get("${sadb}/user/${_username}/uid_number"),
+      managehome => true,
+      shell      => "/bin/bash",
+      groups     => $groups,
+      require    => Class["c2c::skel"],
+    }
+  }
+
+  ssh_authorized_key{"${email}-on-${_onuser}":
+    ensure => $_ensure,
+    target => $target? {false => undef, default => $target },
+    user   => $target? {false => $_onuser, default => undef },
+    type   => $type,
+    key    => $key,
+  }
+
+}
