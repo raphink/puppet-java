b99a04037ebf6e9198a88c4907ad972ced210cf1
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2012-03-29 11:44:47 +0200

    generic-tmpl: rename and split os::user
    The user is split in a base definition which calls a datasource dependent
    backend definition. Currently we only have the sadb backend.

--- manifests/os/user/sadb.pp
+++ manifests/os/user/sadb.pp
@@ -1,51 +1,10 @@
-# == Define: generic-tmpl::c2c::user
+# == Define: generic-tmpl::os::user::sadb
 #
-# Creates a new system user or add an ssh key to an existing user based on
-# Camptocamp's data source (currently SADB).
+# User management backend using SADB.
 #
-# Currently, if the ssh key type is set to none, the user's account and/or its
-# ssh key are removed.
+# Don't use this definition directly unless you know what you're doint.
 #
-# === Parameters
-#
-# [*ensure*]
-#   'present' or 'absent'. Creates or removes the user / the key.
-#
-# [*username*]
-#   *namevar*: Name of the system user to create or whose ssh key should be
-#   installed on another account. If the 'onuser' parameter is not set or is
-#   equal to 'username', and the account isn't already defined, this defines
-#   it.
-#
-# [*groups*]
-#   If the user has to be defined, it will be added to this groups.
-#
-# [*onuser*]
-#   Optional. Set this to the existing system account on which you want to add
-#   the ssh key. Only useful if it's different from 'username'.
-#
-# [*target*]
-#   Optional. Target location for the ssh key. Defaults the the user's
-#   ~/.ssh/authorized_keys file. See the ssh_authorized_key's documentation
-#   for more information.
-#
-# === Examples
-#
-# Create a nominative system account with the user's ssh key:
-#
-# generic-tmpl::c2c::user {'ckaenzig':
-#   ensure => present,
-#   groups => ['sysadmin'],
-# }
-#
-# Add a user's key in a shared system account:
-#
-# generic-tmpl::c2c::user {'ochriste':
-#   ensure => present,
-#   onuser => 'admin':
-# }
-#
-define generic-tmpl::c2c::user (
+define generic-tmpl::os::user::sadb (
   $ensure   = present,
   $username = false,
   $groups   = false,
