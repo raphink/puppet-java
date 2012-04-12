2891c25dc839f7fa5843c5e8323518f3f8d0480d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-04-12 18:20:00 +0200

    manifests/os-tmpl/os-base.pp - do not declare backport-boto class, use generic-tmpl::backport::boto modules/generic-tmpl/manifests/backport/boto.pp - include aws::boto, do not declare package

--- manifests/backport/boto.pp
+++ manifests/backport/boto.pp
@@ -1,8 +1,8 @@
 class generic-tmpl::backport::boto {
+  include aws::boto
+
   apt::preferences {'python-boto':
     pin => 'release o=Camptocamp',
     priority => 1100,
   }
-  
-  package {'python-boto': }
 }
