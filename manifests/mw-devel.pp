31ac789e3443c1e377e05e5b417adae2edecb312
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-04-24 14:40:13 +0200

    generic-tmpl::mw-devel - removed duplicated ack-grep

--- manifests/mw-devel.pp
+++ manifests/mw-devel.pp
@@ -1,7 +1,6 @@
 class generic-tmpl::mw-devel {
 
   package {
-    "ack-grep":      ensure => present; # a grep replacement
     "ipython":       ensure => present; # interactive python shell
     "libxml2-utils": ensure => present; # xml utilities
     "pyflakes":      ensure => present; # python static code checker
