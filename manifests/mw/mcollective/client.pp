8ff60b0d48a326f5cf784ea00b7e4ef90f85ec86
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-04-25 16:11:48 +0200

    Mcollective packages are not split onRedHat

--- manifests/mw/mcollective/client.pp
+++ manifests/mw/mcollective/client.pp
@@ -1,17 +1,30 @@
 class generic-tmpl::mw::mcollective::client {
   include ::mcollective::client
 
-  $agents = [
-    'augeasquery-client',
-    'filemgr-client',
-    'nettest-client',
-    'nrpe-client',
-    'package-client',
-    'process-client',
-    'puppetd-client',
-    'service-client',
-    'stomputil-client',
-    ]
+  $agents = $::operatingsystem ? {
+    /Debian|Ubuntu/ => [
+      'augeasquery-client',
+      'filemgr-client',
+      'nettest-client',
+      'nrpe-client',
+      'package-client',
+      'process-client',
+      'puppetd-client',
+      'service-client',
+      'stomputil-client',
+      ],
+    /RedHat|CentOS/ => [
+      'augeasquery',
+      'filemgr',
+      'nettest',
+      'nrpe',
+      'package',
+      'process',
+      'puppetd',
+      'service',
+      'stomputil',
+      ],
+  }
 
   mcollective::plugin { $agents:
     ensure => present,
