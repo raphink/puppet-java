927ea80c04648c81c6bb419acd8e344e6b9a0a5b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-05-10 15:56:55 +0200

    Adapt generic-tmpl::backport::php52::sig to install the new mapserver packages

--- manifests/backport/php52/sig.pp
+++ manifests/backport/php52/sig.pp
@@ -1,16 +1,16 @@
 class generic-tmpl::backport::php52::sig inherits generic-tmpl::backport::php52 {
 
   if ($lsbdistcodename == 'squeeze') {
-    apt::sources_list {"c2c-lenny-${repository}-sig":
+    # Use specific mapserver version built for PHP 5.2 and Gdal 1.8
+    apt::sources_list {"c2c-${lsbdistcodename}-${repository}-mapserver-php-5.2":
       ensure  => present,
-      content => "deb http://pkg.camptocamp.net/${repository} lenny sig sig-non-free",
+      content => "deb http://pkg.camptocamp.net/${repository} ${lsbdistcodename} mapserver-php-5.2",
       require => Apt::Key["5C662D02"],
     }
 
-    # Downgrade to lenny version
-    apt::preferences {'php5-mapscript-lenny':
-      package  => 'php5-mapscript',
-      pin      => 'version 5.0.3*',
+    apt::preferences {'mapserver-php52':
+      package  => '*',
+      pin      => 'release c=mapserver-php-5.2',
       priority => '1100',
     }
   }
