e8b574f22144b820e94acbd0bcfa51419b8685c5
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2012-06-28 16:41:29 +0200

    generic-tmpl::mw::postgresql::9 - illegal class name - renamed to generic-tmpl::mw::postgresql::v9

---
+++ manifests/mw/postgresql/v9.pp
@@ -0,0 +1,37 @@
+class generic-tmpl::mw::postgresql::v9 {
+
+  # avoid partial configuration on untested-distribution
+  if $::lsbdistcodename !~ /^squeeze$/ {
+    fail "${name} not tested on $::operatingsystem/$::lsbdistcodename"
+  }
+
+  $version = $postgresql_version ? {
+    ''      => '9.0',
+    default => $postgresql_version
+  }
+
+  case $version {
+    '9.0'   : { include postgresql::v9-0 }
+    '9.1'   : { include postgresql::v9-1 }
+    default : { fail "unsupported version ${version}" }
+  }
+
+  include postgresql::backup
+  include postgresql::administration
+
+  if !defined(Package['python-psycopg2']) {
+    package {'python-psycopg2':
+      ensure => present,
+    }
+  }
+
+  apt::preferences {'libpq-dev':
+    pin      => 'release a=squeeze-backports',
+    priority => '1100',
+  }
+
+  package {"postgresql-plperl-${version}":
+    ensure => present,
+  }
+
+}
