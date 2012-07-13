0c5391890c21477ea581ae799bd547f5e8891c34
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-07-13 15:25:42 +0200

    Add apt::preferences for sencha

--- manifests/mw/sencha.pp
+++ manifests/mw/sencha.pp
@@ -1,11 +1,17 @@
 class generic-tmpl::mw::sencha {
   apt::sources_list {"c2c-${lsbdistcodename}-${repository}-sencha":
     ensure  => present,
-    content => "deb http://sencha:eev1yoL4Ta@pkg.camptocamp.net/${repository} ${lsbdistcodename} sencha\n",
+    content => "deb http://sencha:eev1yoL4Ta@sencha.pkg.camptocamp.net/${repository} ${lsbdistcodename} sencha\n",
   }
 
   package {'sencha-sdk-tools':
     ensure  => present,
     require => Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sencha"],
+  }
+
+  apt::preferences {'sencha':
+    package  => 'sencha-sdk-tools',
+    pin      => "release o=Camptocamp",
+    priority => 1100,
   }
 }
