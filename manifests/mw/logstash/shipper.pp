baa7c1e3cb81df9d19de8952afb7697c659c85a9
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2012-08-23 10:05:59 +0200

    this limit is now included directly in the debian package

--- manifests/mw/logstash/shipper.pp
+++ manifests/mw/logstash/shipper.pp
@@ -3,11 +3,6 @@ class generic-tmpl::mw::logstash::shipper {
   include ::logstash
   include ::logstash::shipper
 
-  augeas {'limit logstash-shipper memory':
-    changes => 'set /files/etc/default/logstash-shipper/LS_JAVA_OPTS \'"-Xms256m -Xmx256m"\'',
-    notify  => Service['logstash-shipper'],
-  }
-
   apt::preferences{'libpcre3':
     pin      => 'release c=sysadmin, o=Camptocamp',
     priority => '1001',
