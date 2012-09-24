a5dc636dfd93917c595642f16e67409dea1f1663
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-09-24 10:35:31 +0200

    Add apt::preferences for compass

--- manifests/mw/compass.pp
+++ manifests/mw/compass.pp
@@ -1,6 +1,13 @@
 class generic-tmpl::mw::compass {
   package {'libcompass-ruby1.8':
-    ensure  => present,
+    ensure  => latest,
     require => Apt::Sources_list["c2c-${lsbdistcodename}-${repository}-sig"],
+  }
+
+  apt::preferences {'compass':
+    ensure   => present,
+    package  => 'libcompass-ruby1.8',
+    pin      => 'release o=Camptocamp',
+    priority => '1100',
   }
 }
