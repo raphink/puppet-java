9ad1deb1302d1365fe11b2312a8d85b8446db18f
Marc Fournier <marc.fournier@camptocamp.com>
2012-10-16 11:13:31 +0200

    fixed path to collectd lens.

--- manifests/mw/collectd/lens.pp
+++ manifests/mw/collectd/lens.pp
@@ -1,6 +1,6 @@
 class generic-tmpl::mw::collectd::lens {
   augeas::lens {'collectd':
-    lens_source => 'puppet:///modules/generic-tmpl/lenses/collectd.aug',
-    test_source => 'puppet:///modules/generic-tmpl/lenses/test_collectd.aug',
+    lens_source => 'puppet:///modules/generic-tmpl/augeas/collectd.aug',
+    test_source => 'puppet:///modules/generic-tmpl/augeas/test_collectd.aug',
   }
 }
