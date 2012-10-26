c92d2906464a957184da505e30555459ef739e67
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-10-26 11:57:49 +0200

    Don't use version numbers in apt::preferences

--- manifests/mw/augeas/debian.pp
+++ manifests/mw/augeas/debian.pp
@@ -2,18 +2,10 @@ class generic-tmpl::mw::augeas::debian {
 
   include ::augeas::debian
 
-  if ($augeas_version != "present") {
-    apt::preferences {'augeas':
-      package  => 'augeas-lenses augeas-tools libaugeas0',
-      ensure   => present,
-      pin      => "version ${augeas_version}",
-      priority => 1100;
-    }
-
-    apt::preferences {'libaugeas-ruby1.8':
-      ensure   => present,
-      pin      => "version ${augeas_ruby_version}",
-      priority => 1100,
-    }
+  apt::preferences {'augeas':
+    package  => 'augeas-lenses augeas-tools libaugeas0 libaugeas-ruby1.8',
+    ensure   => present,
+    pin      => "release o=Camptocamp",
+    priority => 1100;
   }
 }
