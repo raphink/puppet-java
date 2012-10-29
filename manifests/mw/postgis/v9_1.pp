de6d1c1e3dcd66b490689ed9a0f5d687065229f7
Frederic Junod <frederic.junod@camptocamp.com>
2012-10-29 10:55:55 +0100

    Add generic-tmpl::mw::postgis::v9_1

---
+++ manifests/mw/postgis/v9_1.pp
@@ -0,0 +1,22 @@
+class generic-tmpl::mw::postgis::v9_1 {
+
+  include generic-tmpl::os::pkgrepo::sig
+  include postgis::debian::v9_1
+
+  apt::preferences { ['postgis', 'postgresql-9.1-postgis']:
+    pin      => 'release o=Camptocamp',
+    priority => '1100',
+  }
+
+  # dependencies
+  apt::preferences { ['libgeos-3.3.3', 'libgeos-c1']:
+    pin      => 'release o=Camptocamp',
+    priority => '1100',
+  }
+
+  apt::preferences { ['libproj0', 'proj-data']:
+    pin      => 'release o=Camptocamp',
+    priority => '1100',
+  }
+
+}
