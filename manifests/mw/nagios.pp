7fe3f12815e5c9a4f2d9aad7ce3aa0512d20096e
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-11-19 16:23:07 +0100

    Add nsca backports to generic-tmpl::mw::nagios

--- manifests/mw/nagios.pp
+++ manifests/mw/nagios.pp
@@ -7,6 +7,16 @@ class generic-tmpl::mw::nagios {
       'nagios-plugins',
       'nagios-plugins-standard',
       'nagios-plugins-basic',
+      'nsca',
+    ]:
+        pin      => 'release o=Camptocamp',
+        priority => '1100',
+    }
+  }
+
+  if $::lsbdistcodename == 'wheezy' {
+    apt::preferences {[
+      'nsca',
     ]:
         pin      => 'release o=Camptocamp',
         priority => '1100',
