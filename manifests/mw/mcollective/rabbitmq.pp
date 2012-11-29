50a3d54a6a23ecf1f4763cf69f7eb062b4d98c41
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-11-29 10:59:46 +0100

    Not rabbit@ in node names

--- manifests/mw/mcollective/rabbitmq.pp
+++ manifests/mw/mcollective/rabbitmq.pp
@@ -2,7 +2,7 @@ class generic-tmpl::mw::mcollective::rabbitmq {
   class { '::rabbitmq::server':
     env_config         => 'export SERVER_ERL_ARGS="$SERVER_ERL_ARGS -kernel inet_dist_listen_min 35197 -kernel inet_dist_listen_max 35197 -kernel net_ticktime 600"',
     config_cluster     => true,
-    cluster_disk_nodes => ['rabbit@ip-10-49-21-59', 'rabbit@bgdiback02p'],
+    cluster_disk_nodes => ['ip-10-49-21-59', 'bgdiback02p'],
     erlang_cookie      => 'aeph4eel2Lu5sazeengohcaeDoorieng',
   }
 
