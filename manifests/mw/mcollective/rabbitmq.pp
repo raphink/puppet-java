01224af74d9fdc3bc230a83798f2a9a80ac06576
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-11-29 10:15:42 +0100

    Remove rabbitmq from mcollective and use the rabbitmq module

--- manifests/mw/mcollective/rabbitmq.pp
+++ manifests/mw/mcollective/rabbitmq.pp
@@ -1,5 +1,14 @@
 class generic-tmpl::mw::mcollective::rabbitmq {
-  include ::mcollective::rabbitmq
+  class { '::rabbitmq::server':
+    env_config         => 'export SERVER_ERL_ARGS="$SERVER_ERL_ARGS -kernel inet_dist_listen_min 35197 -kernel inet_dist_listen_max 35197 -kernel net_ticktime 600"',
+    config_cluster     => true,
+    cluster_disk_nodes => ['rabbit@ip-10-49-21-59', 'rabbit@bgdiback02p'],
+    erlang_cookie      => 'aeph4eel2Lu5sazeengohcaeDoorieng',
+  }
+
+  rabbitmq_plugin {
+    ["amqp_client", "rabbitmq_stomp"]: ensure => present,
+  }
 
   case $::operatingsystem {
     /Debian|Ubuntu/: {
