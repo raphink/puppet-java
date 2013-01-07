60b1d540931104d227eac3d4acf74618a7fa2848
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2013-01-07 17:11:20 +0100

    mw::postgresql: new class to replace mw::postgresql::v*
    This class replaces the multiple classes per version. The version of
    postgresql to install is set by a mandatory class parameter.

---
+++ manifests/mw/postgresql.pp
@@ -0,0 +1,29 @@
+class generic-tmpl::mw::postgresql (
+  version
+) {
+
+  class {'::postgresql':
+    version => $version,
+  }
+
+  include ::postgresql::backup
+  include ::postgresql::administration
+
+  if !defined(Package["python-psycopg2"]) {
+    package {"python-psycopg2":
+      ensure => present,
+    }
+  }
+
+  package {"postgresql-plperl-${version}":
+    ensure => present,
+  }
+
+  if $lsbdistcodename =~ /lenny|squeeze/ {
+    apt::preferences {'libpq-dev':
+      pin      => "release a=${lsbdistcodename}-backports",
+      priority => '1100',
+    }
+  }
+
+}
