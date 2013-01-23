57e6dad3825b339b03d931c78c2b835d2dde0ae8
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2013-01-23 16:20:15 +0100

    mw::nagios: apt-preferences stuff is only for debian

--- manifests/mw/nagios.pp
+++ manifests/mw/nagios.pp
@@ -1,24 +1,28 @@
 class generic-tmpl::mw::nagios {
 
-  $common_packages = [
-    'nagios3-core',
-    'nagios3-common',
-    'nagios-plugins',
-    'nagios-plugins-standard',
-    'nagios-plugins-basic'
-  ]
+  if ::osfamily == 'Debian' {
 
-  if $::lsbdistcodename == 'lenny' {
-    $packages = [$common_packages,['nsca']]
-  } elsif $::lsbdistcodename == 'squeeze' {
-    $packages = $common_packages
-  } elsif $::lsbdistcodename == 'wheezy' {
-    $packages = ['nsca']
-  }
+    $common_packages = [
+      'nagios3-core',
+      'nagios3-common',
+      'nagios-plugins',
+      'nagios-plugins-standard',
+      'nagios-plugins-basic'
+    ]
 
-  apt::preferences {$packages:
-    pin      => 'release o=Camptocamp',
-    priority => '1100',
+    if $::lsbdistcodename == 'lenny' {
+      $packages = [$common_packages,['nsca']]
+    } elsif $::lsbdistcodename == 'squeeze' {
+      $packages = $common_packages
+    } elsif $::lsbdistcodename == 'wheezy' {
+      $packages = ['nsca']
+    }
+
+    apt::preferences {$packages:
+      pin      => 'release o=Camptocamp',
+      priority => '1100',
+    }
+
   }
 
   include ::nagios
