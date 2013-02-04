2f4f71c0c38d99a9043a44989c298b39b8b7e707
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-02-04 11:28:43 +0100

    generic-tmpl::mw::mcollective::rabbitmq: copy ssl keys

--- manifests/mw/mcollective/rabbitmq.pp
+++ manifests/mw/mcollective/rabbitmq.pp
@@ -7,25 +7,25 @@ class generic-tmpl::mw::mcollective::rabbitmq (
 ) {
 
   # Copy SSL keys to give them the proper rights
-  #file {
-  #  '/etc/rabbitmq/ssl/ca.pem':
-  #    ensure => file,
-  #    owner  => 'rabbitmq',
-  #    group  => 'rabbitmq',
-  #    source => '/var/lib/puppet/ssl/certs/ca.pem';
+  file {
+    '/etc/rabbitmq/ssl/ca.pem':
+      ensure => file,
+      owner  => 'rabbitmq',
+      group  => 'rabbitmq',
+      source => '/var/lib/puppet/ssl/certs/ca.pem';
 
-  #  "/etc/rabbitmq/ssl/${::fqdn}.crt":
-  #    ensure => file,
-  #    owner  => 'rabbitmq',
-  #    group  => 'rabbitmq',
-  #    source => "/var/lib/puppet/ssl/certs/${::fqdn}.pem";
+    "/etc/rabbitmq/ssl/${::fqdn}.crt":
+      ensure => file,
+      owner  => 'rabbitmq',
+      group  => 'rabbitmq',
+      source => "/var/lib/puppet/ssl/certs/${::fqdn}.pem";
 
-  #  "/etc/rabbitmq/ssl/${::fqdn}.key":
-  #    ensure => file,
-  #    owner  => 'rabbitmq',
-  #    group  => 'rabbitmq',
-  #    source => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem";
-  #}
+    "/etc/rabbitmq/ssl/${::fqdn}.key":
+      ensure => file,
+      owner  => 'rabbitmq',
+      group  => 'rabbitmq',
+      source => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem";
+  }
 
   class { '::rabbitmq::server':
     env_config         => 'export SERVER_ERL_ARGS="$SERVER_ERL_ARGS -kernel inet_dist_listen_min 35197 -kernel inet_dist_listen_max 35197 -kernel net_ticktime 600"',
