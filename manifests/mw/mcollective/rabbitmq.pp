a19ef9c8c145a89f19781172055a63d0ff17b387
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-02-05 14:20:01 +0000

    Let rabbitmq authenticate through LDAP

--- manifests/mw/mcollective/rabbitmq.pp
+++ manifests/mw/mcollective/rabbitmq.pp
@@ -4,6 +4,12 @@ class generic-tmpl::mw::mcollective::rabbitmq (
   $vhost = '/mcollective',
   $user = 'guest',
   $password = 'guest',
+  $ldap_auth=false,
+  $ldap_server=undef,
+  $ldap_user_dn_pattern=undef,
+  $ldap_use_ssl=undef,
+  $ldap_port=undef,
+  $ldap_log=undef,
 ) {
 
   # Copy SSL keys to give them the proper rights
@@ -28,18 +34,24 @@ class generic-tmpl::mw::mcollective::rabbitmq (
   }
 
   class { '::rabbitmq::server':
-    env_config         => 'export SERVER_ERL_ARGS="$SERVER_ERL_ARGS -kernel inet_dist_listen_min 35197 -kernel inet_dist_listen_max 35197 -kernel net_ticktime 600"',
-    config_cluster     => true,
-    cluster_disk_nodes => $cluster_disk_nodes,
-    erlang_cookie      => $erlang_cookie,
-    ssl                => true,
-    ssl_cacert         => '/etc/rabbitmq/ssl/ca.pem',
-    ssl_cert           => "/etc/rabbitmq/ssl/${::fqdn}.crt",
-    ssl_key            => "/etc/rabbitmq/ssl/${::fqdn}.key",
-    config_stomp       => true,
-    stomp_port         => '61613',
-    ssl_stomp_port     => '61614',
-    #require           => [
+    env_config           => 'export SERVER_ERL_ARGS="$SERVER_ERL_ARGS -kernel inet_dist_listen_min 35197 -kernel inet_dist_listen_max 35197 -kernel net_ticktime 600"',
+    config_cluster       => true,
+    cluster_disk_nodes   => $cluster_disk_nodes,
+    erlang_cookie        => $erlang_cookie,
+    ssl                  => true,
+    ssl_cacert           => '/etc/rabbitmq/ssl/ca.pem',
+    ssl_cert             => "/etc/rabbitmq/ssl/${::fqdn}.crt",
+    ssl_key              => "/etc/rabbitmq/ssl/${::fqdn}.key",
+    config_stomp         => true,
+    stomp_port           => '61613',
+    ssl_stomp_port       => '61614',
+    ldap_auth            => $ldap_auth,
+    ldap_server          => $ldap_server,
+    ldap_user_dn_pattern => $ldap_user_dn_pattern,
+    ldap_use_ssl         => $ldap_use_ssl,
+    ldap_port            => $ldap_port,
+    ldap_log             => $ldap_log,
+    #require             => [
     #  File['/etc/rabbitmq/ssl/ca.pem'],
     #  File["/etc/rabbitmq/ssl/${::fqdn}.crt"],
     #  File["/etc/rabbitmq/ssl/${::fqdn}.key"],
