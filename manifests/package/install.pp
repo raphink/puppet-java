77c3733a6b1379c3c0b5c890c37b9cca126596bd
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2013-04-30 09:49:29 +0200

    added new packages

--- manifests/package/install.pp
+++ manifests/package/install.pp
@@ -1,19 +1,24 @@
 class generic-tmpl::package::install {
+  include ::generic-tmpl::package::ackgrep
   include ::generic-tmpl::package::curl
   include ::generic-tmpl::package::cvs
   include ::generic-tmpl::package::dos2unix
   include ::generic-tmpl::package::elinks
   include ::generic-tmpl::package::emacs
   include ::generic-tmpl::package::gnupg
+  include ::generic-tmpl::package::ldaputils
+  include ::generic-tmpl::package::less
   include ::generic-tmpl::package::patch
   include ::generic-tmpl::package::pwgen
   include ::generic-tmpl::package::rsync
   include ::generic-tmpl::package::screen
   include ::generic-tmpl::package::sharutils
   include ::generic-tmpl::package::tmux
+  include ::generic-tmpl::package::tree
   include ::generic-tmpl::package::unix2dos
   include ::generic-tmpl::package::vim
   include ::generic-tmpl::package::wget
+  include ::generic-tmpl::package::xclip
   include ::generic-tmpl::package::zip
 
   Package <| tag == 'common-packages' |>
